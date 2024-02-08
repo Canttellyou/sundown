cbb02c2567439c21e8119a0127cadfe38597ffea
Jason Akbar <akbar123jason@gmail.com>
2024-02-08 21:01:28 +0000

    trial

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
-const Navbar = dynamic(() => import("./Navbar"), {
+const Navbar = dynamic(() => import("@/components/Navbar"), {
   ssr: false,
 });
 
