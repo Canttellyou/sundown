1dcb7a42bb72c4026a5fc9a57ff5209cce777ec7
Jason Akbar <akbar123jason@gmail.com>
2024-02-10 19:23:02 +0000

    video fix

--- components/Homepage/VideoBox.tsx
+++ components/Homepage/VideoBox.tsx
@@ -7,11 +7,8 @@ const VideoBox = () => {
   return (
     <StyledVideoBox>
       <div className="video-container">
-        <video autoPlay loop muted controls={false} playsInline>
-          <source
-            src="https://download-video.akamaized.net/v3-1/playback/bdb3a683-9706-4177-8ca4-48dfe2703fac/c72af10b?__token__=st=1707563484~exp=1707577884~acl=%2Fv3-1%2Fplayback%2Fbdb3a683-9706-4177-8ca4-48dfe2703fac%2Fc72af10b%2A~hmac=abb18b6f2c57eecaf0307fb8c8713e48c74ef7b78cf62f2a820a74274b6ee730&r=dXMtd2VzdDE%3D"
-            type="video/mp4"
-          />
+        <video autoPlay loop muted playsInline>
+          <source src="/hero-video.mp4" type="video/mp4" />
         </video>
       </div>
     </StyledVideoBox>
@@ -28,6 +25,7 @@ const StyledVideoBox = styled.div`
     object-fit: cover;
     border-radius: 1.25em;
     overflow: hidden;
+    min-height: 100vh;
   }
 `;
 
